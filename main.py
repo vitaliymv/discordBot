e257f969c760ff8e3c302158628b2268e1cd88f5
vitaliymv <movcan77@gmail.com>
2022-02-13 21:00:00 +0200

    Add database connection. Get token from admin panel.

--- main.py
+++ main.py
@@ -1,6 +1,37 @@
 import discord
+import pymysql.cursors
+import json
 
 client = discord.Client()
+
+
+def getConnection():
+    connection = pymysql.connect(host='eu-cdbr-west-02.cleardb.net',
+                                 user='ba7528f2bb07ee',
+                                 password='f66383ea',
+                                 db='heroku_37f7ea8f4ada09e',
+                                 charset='utf8mb4',
+                                 cursorclass=pymysql.cursors.DictCursor)
+    return connection
+
+
+print("connect successful!!")
+
+
+def global_paths():
+    global DISCORD_TOKEN
+
+
+try:
+    with getConnection().cursor() as cursor:
+        sql = "select * from system_settings where item = 'common' limit 1;"
+        cursor.execute(sql)
+        result = cursor.fetchone()
+        value = result.get('value')
+        settings = json.loads(value)
+        DISCORD_TOKEN = settings['discord_token']
+finally:
+    getConnection().close()
 
 
 @client.event
@@ -8,7 +39,6 @@ async def on_ready():
     print('We have logged in as {0.user}'.format(client))
     print(client.user.id)
     print(client.user.name)
-
 
 
 @client.event
@@ -23,4 +53,5 @@ async def on_message(message):
         user = await client.fetch_user(message.author.id)
         await user.send("hello")
 
-client.run('OTQyNDY4MDk2NzM4ODc3NTQw.Ygk70A.JxWvQRrtT3CHVF5pn6qIBHyQPtI')
+
+client.run(DISCORD_TOKEN)
