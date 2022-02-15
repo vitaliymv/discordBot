c6142f1fda4066112d07559b8743f9b7f97fe556
vitaliymv <movcan77@gmail.com>
2022-02-15 13:23:51 +0200

    Update music bot

--- main.py
+++ main.py
@@ -83,8 +83,9 @@ async def play(ctx, arg):
     else:
         with YoutubeDL(ydl_opts) as ydl:
             info = ydl.extract_info(arg, download=False)
-            URL = info['formats'][0]['url']
+        URL = info['formats'][0]['url']
 
+        # voice.play(discord.FFmpegPCMAudio(executable="ffmpeg/bin/ffmpeg.exe", source=URL, **FFMPEG_OPTIONS))
         voice.play(discord.FFmpegPCMAudio(URL, **FFMPEG_OPTIONS))
 
         while voice.is_playing():
